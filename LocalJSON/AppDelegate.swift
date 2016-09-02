4f08b4acab888d4ab2b30229dbb5f60d0323e1c7
AustinDev16 <austin.blaser@gmail.com>
2016-09-02 10:29:08 -0600

    Fetch characters working.

--- LocalJSON/AppDelegate.swift
+++ LocalJSON/AppDelegate.swift
@@ -16,6 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let cC = CharacterController()
+        
+        print(cC.charactersArray.count)
+        
+        
         return true
     }
 
