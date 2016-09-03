97c06c7fc1615a33b57c58808ef77e54f8f5e675
AustinDev16 <austin.blaser@gmail.com>
2016-09-02 22:53:18 -0600

    CollectionView setup.

---
+++ LocalJSON/CharacterCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CharacterCollectionViewCell.swift
+//  LocalJSON
+//
+//  Created by Austin Blaser on 9/2/16.
+//  Copyright © 2016 MichaelMecham. All rights reserved.
+//
+
+import UIKit
+
+class CharacterCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var idLabel: UILabel!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+}
